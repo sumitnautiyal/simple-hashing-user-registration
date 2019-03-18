2a066e3961e5ddf790f551942794c46a40a11406
Sumit Nautiyal <sumit.nautiyal54@gmail.com>
2019-03-19 04:45:35 +0530

    sixth commit

--- SendingEmail.php
+++ SendingEmail.php
@@ -1,5 +1,15 @@
 <?php
-mail('example@example.com', 'Confirm Email Signup', 'Please complete the signup process', 'sumit.nautiyal54@gmail.com')
+//mail('example@example.com', 'Confirm Email Signup', 'Please complete the signup process', 'sumit.nautiyal54@gmail.com')
+//WARNING: This is not a secure way to send emails 
+$emailTo="receriverEmail@gmail.com";
+$subject="Confirm Email Signup";
+$body="Please complete the signup process";
+$headers="From:sumit.nautiyal54@gmail.com";
+if (mail ($emailTo,$body,$subject,$headers)) {
+	echo "Mail Sent to your email successfully. Check your Inbox";
+} else {
+	echo "Mail not sent";
+}
 
 
 <?
