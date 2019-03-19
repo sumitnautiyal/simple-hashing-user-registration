3fb448f3becb77d399c90b33c6d3803616dab09e
Sumit Nautiyal <sumit.nautiyal54@gmail.com>
2019-03-20 00:04:07 +0530

    seventh commit

---
+++ Activate.php
@@ -0,0 +1,21 @@
+<?php require_once("Include/Session.php")  ?>
+<?php require_once("Include/Db.php")  ?>
+<?php require_once("Include/Functions.php")  ?>
+
+<?php
+	global $connectingDb;
+	if (isset($_GET['token']) {
+	$urlToken=$_GET['token'];
+	$query="UPDATE registration SET active='ON' WHERE token='urlToken' ";
+	$execute=mysql_query($query);
+		if ($execute) {
+			$_SESSION["successMessage"]="Your account is activated successfully";
+			redirect_to("Login.php");
+		} else {
+			$_SESSION["successMessage"]="Somthing went wrong,. Please try again";
+			redirect_to("Login.php");
+		}
+	}
+
+
+>?
